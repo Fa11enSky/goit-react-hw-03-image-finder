46606cda3480fbf47cf229548f19aeb0ae7488e2
Fa11enSky <133168352+Fa11enSky@users.noreply.github.com>
2023-11-21 00:21:45 +0200

    Update request.js

--- src/service/request.js
+++ src/service/request.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 const KEY = '40758984-7f24ff10d45b7d0fc8545c88b';
 axios.defaults.baseURL = 'https://pixabay.com';
-async function requestImage(params) {
-  params = {
-    q: params.query,
+async function requestImage({ query, page }) {
+  const params = {
+    q: query,
     key: KEY,
     image_type: 'photo',
     orientation: 'horizontal',
     per_page: 12,
-    page: params.page,
+    page,
   };
 
   const images = await axios.get('/api/', { params });
